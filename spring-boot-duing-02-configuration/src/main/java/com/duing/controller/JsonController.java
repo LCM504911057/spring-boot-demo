7648a14b95976a543291f9e84bef2f3f5d978e32
HeXie <504911057@qq.com>
2020-09-17 21:34:07 +0800

    1.0-demo

---
+++ spring-boot-duing-02-configuration/src/main/java/com/duing/controller/JsonController.java
@@ -0,0 +1,40 @@
+package com.duing.controller;
+
+import com.duing.bean.Food;
+import com.duing.config.FoodConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RestController
+ * 是
+ * @Controller和@ResponseBody的组合
+ * 返回json形式的响应结果   而不是试图(jsp html)
+ */
+@RestController
+public class JsonController {
+
+    /**
+     * food.rice=rice
+     * food.meat=meat
+     * @Value 通过${}结构获得配置文件中的属性值
+     */
+//    @Value("${food.rice}")
+//    private String rice;
+//    @Value("${food.meat}")
+//    private String meat;
+
+    @Autowired
+    private FoodConfig foodConfig;
+
+    @RequestMapping("/json")
+    public Food json() {
+        Food food = new Food();
+        food.setMeat(foodConfig.getMeat());
+        food.setRice(foodConfig.getRice());
+        return food;
+    }
+
+}
